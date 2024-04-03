9e6b646a975471480036a5ab6715d1e70d82bf8e
AndreGomes10 <andrezinholg10@gmail.com>
2024-04-03 10:40:29 -0300

    Project completed

--- backend/helpers/verify-token.js
+++ backend/helpers/verify-token.js
@@ -1,4 +1,24 @@
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
+
+// middleware to validate token
+const checkToken = (req, res, next) => {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) return res.status(401).json({ message: "Acesso negado!" });
+
+  try {
+    const verified = jwt.verify(token, "nossosecret");
+    req.user = verified;
+    next(); // to continue the flow
+  } catch (err) {
+    res.status(400).json({ message: "O Token é inválido!" });
+  }
+};
+
+module.exports = checkToken;
+
+/*const jwt = require('jsonwebtoken')
 const getToken = require('./get-token')
 
 // middleware to validate token
@@ -25,3 +45,4 @@ const checkToken = (req, res, next) => {
 }
 
 module.exports = checkToken
+*/
